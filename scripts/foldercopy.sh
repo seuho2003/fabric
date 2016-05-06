a4137eab495649cb047410bfb6daff453cfee0d4
rameshthoomu <rameshbabu.thoomU@gmail.com>
2016-05-06 13:04:17 -0400

    Created new folder

--- scripts/foldercopy.sh
+++ scripts/foldercopy.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
+
 if [ "$1" = "false" ] && [ "$2" != "hyperledger" ]; then
+
 rm -rf $HOME/gopath/src/github.com/hyperledger/
-echo "Deleted hyperledger folder"
+
+echo "creating fabric folder to copy files"
+
 mkdir -p $HOME/gopath/src/github.com/hyperledger/fabric
-echo "folder created"
+
+echo "hyperledger/fabric folder created"
+
 ls $HOME/gopath/src/github.com/$2/$3/
+
 cp -r $HOME/gopath/src/github.com/$2/$3/* $HOME/gopath/src/github.com/hyperledger/fabric/
-#mv $HOME//gopath/src/github.com/hyperledger/$3 $HOME/gopath/src/github.com/hyperledger/fabric
+
 ls $HOME/gopath/src/github.com/hyperledger/fabric/
-echo "Copied User Directory into hyperledger"
+
+echo "Copied User directory into hyperledger/fabric folder"
+
 elif [ "$2" != "hyperledger" ]; then
+
 mkdir -p $HOME/gopath/src/github.com/hyperledger/fabric
+
 echo "folder created"
+
 ls $HOME/gopath/src/github.com/$2/$3/
+
 cp -r $HOME/gopath/src/github.com/$2/$3/* $HOME/gopath/src/github.com/hyperledger/fabric/
-mv $HOME/gopath/src/github.com/$2 $HOME/gopath/src/github.com/hyperledger
+
 fi
 
