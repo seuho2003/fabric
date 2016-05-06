3cb7025c05435e7ce4756bbaf60da525c600c2d8
rameshthoomu <rameshbabu.thoomU@gmail.com>
2016-05-06 16:23:55 -0400

    fix for user repo issue

--- scripts/foldercopy.sh
+++ scripts/foldercopy.sh
@@ -3,30 +3,19 @@
 if [ "$1" = "false" ] && [ "$2" != "hyperledger" ]; then
 
 rm -rf $HOME/gopath/src/github.com/hyperledger/
-
 echo "creating fabric folder to copy files"
-
 mkdir -p $HOME/gopath/src/github.com/hyperledger/fabric
-
 echo "hyperledger/fabric folder created"
 
-ls $HOME/gopath/src/github.com/$2/$3/
-
 cp -r $HOME/gopath/src/github.com/$2/$3/* $HOME/gopath/src/github.com/hyperledger/fabric/
 
-ls $HOME/gopath/src/github.com/hyperledger/fabric/
-
-echo "Copied User directory into hyperledger/fabric folder"
-
+echo "Copied $2 files into hyperledger/fabric folder"
 elif [ "$2" != "hyperledger" ]; then
-
 mkdir -p $HOME/gopath/src/github.com/hyperledger/fabric
-
-echo "folder created"
-
-ls $HOME/gopath/src/github.com/$2/$3/
+echo "hyperledger/fabric folder created"
 
 cp -r $HOME/gopath/src/github.com/$2/$3/* $HOME/gopath/src/github.com/hyperledger/fabric/
+echo "Copied $2 files into hyperledger/fabric folder"
 
 fi
 
