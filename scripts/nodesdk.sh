77bc088d8364360e5bdb8a9fd7bff1bca36c0190
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-07-15 02:58:52 +0000

    nodesdk unit test automation

---
+++ scripts/nodesdk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export NUM_FILES_CHANGED=`git diff --name-status -- sdk/node | wc -l`
+echo "Number of files changed in sdk/node directory:" $NUM_FILES_CHANGED
+export NUM_PROTO_FILES_CHANGED=`git diff --name-status -- protos | wc -l`
+echo "Number of Proto files changed are:" $NUM_PROTO_FILES_CHANGED
+if [[ "$NUM_FILES_CHANGED" -eq 0 && "$NUM_PROTO_FILES_CHANGED" -eq 0 ]]; then
+echo "Node sdk files are not changed!";
+exit 0
+fi
+curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/sdk/node/package.json -o package.json
+export NPM_PROD_PACKAGE_VERSION=`cat package.json | grep version | awk -F\" '{ print $4 }'`
+echo "Version Number in Production is:" $NPM_PROD_PACKAGE_VERSION
+cd sdk/node
+export NPM_PR_PACKAGE_VERSION=`cat package.json | grep version | awk -F\" '{ print $4 }'`
+echo "Version number in PR is:" $NPM_PR_PACKAGE_VERSION
+if [[ "$NPM_PROD_PACKAGE_VERSION" = "$NPM_PR_PACKAGE_VERSION" || NPM_PR_PACKAGE_VERSION -lt $NPM_PROD_PACKAGE_VERSION ]];  then
+ echo "Package Version has to change, Please run npm version <major> <minor> <patch>"
+ exit 1
+fi
+echo "npm version and npm publish has to run in Merge"
