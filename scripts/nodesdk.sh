dccdb6d158d0eccef8decb95c70cc31f03d1b761
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-07-18 17:47:04 +0000

    updated nodesdk tests

---
+++ scripts/nodesdk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -xe
+#NUM_FILES_CHANGED=$(git diff --name-status -- sdk/node | wc -l)
+#git show --name-only --oneline --relative=sdk/node
+#NUM_FILES_CHANGED=$(git show --name-only --oneline --relative=sdk/node | wc -l)
+git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master)
+NUM_FILES_CHANGED=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master) | grep "sdk/node" | wc -l
+echo "Number of files changed in sdk/node directory:" $NUM_FILES_CHANGED
+NUM_PROTO_FILES_CHANGED=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master) | grep "protos" | wc -l
+echo "Number of Proto files changed are:" $NUM_PROTO_FILES_CHANGED
+if [[ "$NUM_FILES_CHANGED" -eq 0 && "$NUM_PROTO_FILES_CHANGED" -eq 0 ]]; then
+echo "Node sdk files are not changed!";
+exit 0
+fi
+curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/sdk/node/package.json -o package.json
+NPM_PROD_PACKAGE_VERSION=$(cat package.json | grep version | awk -F\" '{ print $4 }')
+echo "Version Number in Production is:" $NPM_PROD_PACKAGE_VERSION
+cd sdk/node
+NPM_PR_PACKAGE_VERSION=$(cat package.json | grep version | awk -F\" '{ print $4 }')
+echo "Version number in PR is:" $NPM_PR_PACKAGE_VERSION
+if test "$NPM_PROD_PACKAGE_VERSION" = "$NPM_PR_PACKAGE_VERSION";  then
+ echo "Package Version has to change, Please run npm version <major> <minor> <patch>"
+ exit 1
+fi
+echo "npm version and npm publish has to run in Merge"
