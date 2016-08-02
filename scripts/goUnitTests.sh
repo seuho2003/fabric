ebe0c32c323c99e63d62b685ad2eecb1d3988e52
Ramesh Babu <rameshbabu.thoomu@gmail.com>
2016-08-02 11:08:52 -0400

    Update goUnitTests.sh

--- scripts/goUnitTests.sh
+++ scripts/goUnitTests.sh
@@ -21,5 +21,5 @@ echo "DONE!"
 echo "Running tests..."
 #go test -cover -p 1 -timeout=20m $PKGS | go-junit-report > report.xml
 #gocov test -cover -p 1 -timeout=20m $PKGS | gocov-xml > coverage.xml
-#gocov test -cover -p 1 -timeout=20m $PKGS | gocov-xml > report.xml
-go test -cover -p 1 -timeout=20m $PKGS
+go test -coverprofile -p 1 -timeout=20m $PKGS | gocov-xml > report.xml
+#go test -cover -p 1 -timeout=20m $PKGS
