e0057e4c529022907ef359bf983903bd9f6e47c0
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-04-20 02:30:54 +0000

    download docker images from dockerhub
    This patch provides flexbility to download docker images
    based on user choice, tag it as hyperledger/fabric-image:latest
    and remove downloaded docker images. This gives great flexibility
    for user not to modify docker-compose file with specific tags.
    
    ex:
    ./download-dockerimages.sh -c <ca tag> -f <fabric-tag> -o <org-name>
    ./download-dockerimages.sh -c x86_64-1.0.0-alpha -f x86_64-1.0.0-alpha -o hyperledger
    
    Change-Id: I83788bc613461a0775a898173c6239d5f40e667d
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- examples/e2e_cli/download-dockerimages.sh
+++ examples/e2e_cli/download-dockerimages.sh
@@ -8,20 +8,40 @@
 
 dockerFabricPull() {
   local FABRIC_TAG=$1
+  local ORG_NAME=$2
   for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper; do
       echo "==> FABRIC IMAGE: $IMAGES"
       echo
-      docker pull hyperledger/fabric-$IMAGES:$FABRIC_TAG
-      docker tag hyperledger/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES
+    if [ "${ORG_NAME}" == "hyperledger" ]; then
+      echo "========> from $ORG_NAME" docker hub
+      docker pull $ORG_NAME/fabric-$IMAGES:$FABRIC_TAG
+      docker tag $ORG_NAME/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES:$FABRIC_TAG
+      docker tag $ORG_NAME/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES
+    else
+      echo "========> from $ORG_NAME" docker hub
+      docker pull $ORG_NAME/fabric-$IMAGES-$ARCH:$FABRIC_TAG
+      docker tag $ORG_NAME/fabric-$IMAGES-$ARCH:$FABRIC_TAG hyperledger/fabric-$IMAGES:$FABRIC_TAG
+      docker tag $ORG_NAME/fabric-$IMAGES-$ARCH:$FABRIC_TAG hyperledger/fabric-$IMAGES
+      docker rmi -f $ORG_NAME/fabric-$IMAGES-$ARCH:$FABRIC_TAG
+    fi
   done
 }
 
 dockerCaPull() {
       local CA_TAG=$1
+      local ORG_NAME=$2
       echo "==> FABRIC CA IMAGE"
       echo
-      docker pull hyperledger/fabric-ca:$CA_TAG
-      docker tag hyperledger/fabric-ca:$CA_TAG hyperledger/fabric-ca
+    if [ "${ORG_NAME}" == "hyperledger" ]; then
+      docker pull $ORG_NAME/fabric-ca:$CA_TAG
+      docker tag $ORG_NAME/fabric-ca:$CA_TAG hyperledger/fabric-ca:$CA_TAG
+      docker tag $ORG_NAME/fabric-ca:$CA_TAG hyperledger/fabric-ca
+    else
+      docker pull $ORG_NAME/fabric-ca-$ARCH:$CA_TAG
+      docker tag $ORG_NAME/fabric-ca-$ARCH:$CA_TAG hyperledger/fabric-ca
+      docker tag $ORG_NAME/fabric-ca-$ARCH:$CA_TAG hyperledger/fabric-ca:$CA_TAG
+      docker rmi -f $ORG_NAME/fabric-ca-$ARCH:$CA_TAG
+    fi
 }
 usage() {
       echo "Description "
@@ -31,27 +51,29 @@ usage() {
       echo
       echo "USAGE: "
       echo
-      echo "./download-dockerimages.sh [-c <fabric-ca tag>] [-f <fabric tag>]"
+      echo "./download-dockerimages.sh [-c <fabric-ca tag>] [-f <fabric tag>] [-o <dockerhub org name>]"
       echo "      -c fabric-ca docker image tag"
       echo "      -f fabric docker image tag"
-      echo
+      echo "      -o orgnization name"
       echo
       echo "EXAMPLE:"
-      echo "./download-dockerimages.sh -c x86_64-1.0.0-alpha -f x86_64-1.0.0-alpha"
+      echo "./download-dockerimages.sh -c x86_64-1.0.0-alpha -f x86_64-1.0.0-alpha -o hyperledger"
       echo
       echo "By default, pulls fabric-ca and fabric 1.0.0-alpha docker images"
       echo "from hyperledger dockerhub"
       exit 0
 }
 
-while getopts "\?hc:f:" opt; do
+while getopts "\?hc:f:o:" opt; do
   case "$opt" in
      c) CA_TAG="$OPTARG"
         echo "Pull CA IMAGES"
         ;;
-
      f) FABRIC_TAG="$OPTARG"
         echo "Pull FABRIC TAG"
+        ;;
+     o) ORG_NAME="$OPTARG"
+        echo "ORG NAME"
         ;;
      \?|h) usage
         echo "Print Usage"
@@ -61,12 +83,16 @@ done
 
 : ${CA_TAG:="x86_64-1.0.0-alpha"}
 : ${FABRIC_TAG:="x86_64-1.0.0-alpha"}
+: ${ORG_NAME:="hyperledger"}
+
+ARCH=$(uname -m)
+echo "===========> $ARCH"
 
 echo "===> Pulling fabric Images"
-dockerFabricPull ${FABRIC_TAG}
+dockerFabricPull ${FABRIC_TAG} ${ORG_NAME}
 
 echo "===> Pulling fabric ca Image"
-dockerCaPull ${CA_TAG}
+dockerCaPull ${CA_TAG} ${ORG_NAME}
 echo
 echo "===> List out hyperledger docker images"
 docker images | grep hyperledger*
