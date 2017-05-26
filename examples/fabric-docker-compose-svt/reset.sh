b5020d9a98d78a592d412d2e87b75f3e801a6695
Xi Xue Jia <xixuejia@gmail.com>
2017-05-25 23:21:29 -0400

    delete $tmp/hfc directory when resetting network
    Change-Id: I20feb3396c358c03b8a3199d6e6594b0a6751e13
    Signed-off-by: Xi Xue Jia <xixuejia@gmail.com>

--- examples/fabric-docker-compose-svt/reset.sh
+++ examples/fabric-docker-compose-svt/reset.sh
@@ -15,8 +15,10 @@ function rmImages() {
     docker rmi -f $(docker images|grep $keyword|awk '{print $3}')
 }
 
+echo "deleting all docker containers..."
 rmAllContainers
 # remove all docker images that built for chaincodes
+echo "removing all docker images whose name contains dev"
 rmImages dev
 GOPATH=$GOPATH
 : ${GOPATH:="/opt/gopath"}
@@ -29,8 +31,19 @@ rm -rf ./crypto-config/*
 cd $GOPATH/src/github.com/hyperledger/fabric/examples/fabric-docker-compose-svt/
 ./network_setup.sh -s -n myhello -f docker-compose-e2e.yaml up
 rm -rf ~/.hfc*
-rm -rf /tmp/hfc*
-# rm -rf /var/folders/*
+# remove hfc directory in tmp
+if test -d "$TMPDIR"; then
+    :
+elif test -d "$TMP"; then
+    TMPDIR=$TMP
+elif test -d /var/tmp; then
+    TMPDIR=/var/tmp
+else
+    TMPDIR=/tmp
+fi
+echo "temp directory for the OS is:" $TMPDIR
+echo "deleting hfc directory: "$TMPDIR"hfc"
+rm -rf $TMPDIR/hfc
 cd $GOPATH/src/github.com/hyperledger/fabric-sdk-node/test/fixtures/channel
 rm -rf *
 echo "begin to copy the crypto stuffs"
