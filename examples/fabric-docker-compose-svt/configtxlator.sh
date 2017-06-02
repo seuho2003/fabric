8789635607f9a989abe7a7a867e007eb89218101
Xi Xue Jia <xixuejia@gmail.com>
2017-06-02 16:58:00 -0400

    Update ChannelCreationPolicy to require All organizations' admins signatures
    Change-Id: I18f1fe024b61bef4135053eb8903248e933190a1
    Signed-off-by: Xi Xue Jia <xixuejia@gmail.com>

--- examples/fabric-docker-compose-svt/configtxlator.sh
+++ examples/fabric-docker-compose-svt/configtxlator.sh
@@ -29,13 +29,19 @@ bigMsg "Decoding genesis block"
 
 curl -X POST --data-binary @./channel-artifacts/default.orderer.genesis.block http://127.0.0.1:7059/protolator/decode/common.Block > ./channel-artifacts/default_genesis_block.json || die "Error decoding genesis block"
 
+bigMsg "Copying default_genesis_block.json to updated_genesis_block.json"
+
+cp ./channel-artifacts/default_genesis_block.json ./channel-artifacts/updated_genesis_block.json || die "Error in copying genesis block json"
+
 bigMsg "Updating genesis config"
 
 # Basically we can customize anything to override default values and then encode the customized json into orderer genesis block
-jq ".data.data[0].payload.data.config.channel_group.groups.Orderer.values.BatchSize.value.maxMessageCount = 20" ./channel-artifacts/default_genesis_block.json > ./channel-artifacts/updated_genesis_block.json
+#jq ".data.data[0].payload.data.config.channel_group.groups.Orderer.values.BatchSize.value.maxMessageCount = 20" ./channel-artifacts/default_genesis_block.json > ./channel-artifacts/updated_genesis_block.json
+# Update ChannelCreationPolicy
+jq ".data.data[0].payload.data.config.channel_group.groups.Consortiums.groups.SampleConsortium.values.ChannelCreationPolicy.value.policy.rule=\"ALL\"" ./channel-artifacts/default_genesis_block.json > ./channel-artifacts/updated_genesis_block.json
 
 bigMsg "Re-encoding the updated genesis block"
 
-curl -X POST --data-binary @./channel-artifacts/updated_genesis_block.json http://127.0.0.1:7059/protolator/encode/common.Block > ./channel-artifacts/orderer.genesis.proto
+curl -X POST --data-binary @./channel-artifacts/updated_genesis_block.json http://127.0.0.1:7059/protolator/encode/common.Block > ./channel-artifacts/orderer.genesis.block
 
 bigMsg "Bootstrapping edit complete"
