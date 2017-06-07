958158974d5c7caef03548e23333fd8cde178fe3
Ramesh Babu <rameshbabu.thoomu@gmail.com>
2017-06-07 10:39:56 -0400

    Update bootstrap-1.0.0-alpha2.sh

--- scripts/bootstrap-1.0.0-alpha2.sh
+++ scripts/bootstrap-1.0.0-alpha2.sh
@@ -5,34 +5,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-export VERSION=1.0.0-alpha2
+export VERSION=1.0.0-alpha3
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
 #Set MARCH variable i.e ppc64le,s390x,x86_64,i386
 MARCH=`uname -m`
 
 dockerFabricPull() {
-  local FABRIC_TAG=$1
+  local FABRIC_TAG=x86_64-1.0.0-alpha3-snapshot-52853f8
   for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper; do
       echo "==> FABRIC IMAGE: $IMAGES"
       echo
-      docker pull hyperledger/fabric-$IMAGES:$FABRIC_TAG
-      docker tag hyperledger/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES
+      docker pull rameshthoomu/fabric-$IMAGES-x86_64:$FABRIC_TAG
+      docker tag rameshthoomu/fabric-$IMAGES-x86_64:$FABRIC_TAG hyperledger/fabric-$IMAGES
+      docker tag rameshthoomu/fabric-$IMAGES-x86_64:$FABRIC_TAG hyperledger/fabric-$IMAGES:$FABRIC_TAG
   done
 }
 
 dockerCaPull() {
-      local CA_TAG=$1
+      local CA_TAG=x86_64-1.0.0-alpha3-snapshot-c9372be
       echo "==> FABRIC CA IMAGE"
       echo
-      docker pull hyperledger/fabric-ca:$CA_TAG
-      docker tag hyperledger/fabric-ca:$CA_TAG hyperledger/fabric-ca
+      docker pull rameshthoomu/fabric-ca-x86_64:$CA_TAG
+      docker tag rameshthoomu/fabric-ca-x86_64:$CA_TAG hyperledger/fabric-ca:$CA_TAG
 }
 
 : ${CA_TAG:="$MARCH-$VERSION"}
 : ${FABRIC_TAG:="$MARCH-$VERSION"}
 
 echo "===> Downloading platform binaries"
-curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/fabric-binary/${ARCH}-${VERSION}/fabric-binary-${ARCH}-${VERSION}.tar.gz | tar xz
+curl https://nexus.hyperledger.org/content/repositories/snapshots/org/hyperledger/fabric/fabric-binary/${ARCH}-${VERSION}-SNAPSHOT/fabric-binary-${ARCH}-${VERSION}-20170606.184514-5.tar.gz | tar xz
 
 echo "===> Pulling fabric Images"
 dockerFabricPull ${FABRIC_TAG}
